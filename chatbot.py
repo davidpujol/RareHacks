e8fa978f58be3487f54fd72a37ee8c152aefee57
David Pujol <david.pujolperich@gmail.com>
2019-07-05 18:50:04 +0200

    Example of CNN

--- chatbot.py
+++ chatbot.py
@@ -1,11 +1,78 @@
+#telegram api
 from telegram.ext import Updater
 from telegram.ext import MessageHandler, Filters
 
+#nltk
+import nltk
+import numpy as np
+import random
+import string # to process standard python strings
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from nltk import pos_tag
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+# GREETINGS
+GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
+GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
+
+def greeting(sentence):
+    """If user's input is a greeting, return a greeting response"""
+    for word in sentence.split():
+        if word.lower() in GREETING_INPUTS:
+            return random.choice(GREETING_RESPONSES)
+
+
+#LEMMATIZATION
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)	#remove punctuation
+
+#funtion to lemmatize a sentance
+lemmer = WordNetLemmatizer()	#this is an internal dictionary
+def lemmatize(p):
+    if p[1][0] in {'N','V'}:
+        return lemmer.lemmatize(p[0].lower(), pos=p[1][0].lower())
+    return p[0]
+
+
+def removeStopWords(sentance):
+    words = word_tokenize(sentance)
+    filtered_words = [word for word in word_list if word not in stopwords.words('english')]
+    return words
+
+def treatInput(sentence):
+    sentence = sentence.lower().translate(remove_punct_dict)    #eliminem els punts
+    print(sentence)
+    sentence2 = removeStopWords(sentence)
+    print(sentence2)
+    pairs = pos_tag(sentence)
+    print(pairs)
+
+
 #function to be called
 def responde(bot, update):
-    bot.send_message(chat_id = update.message.chat_id, text = "Hello World")
+    user_response = update.message.text
+
+    if (user_response != 'bye' or user_response != 'Bye'):
+            if (user_response == 'thanks' or user_response == 'thank you'):
+                bot.send_message(chat_id=update.message.chat_id, text="You are very welcome")
+
+            else:
+                if (greeting(user_response) != None):
+                    bot.send_message(chat_id=update.message.chat_id, text=greeting(user_response))
+                else:
+                    treatInput(user_response)
+                    bot.send_message(chat_id = update.message.chat_id, text="OTHER")
+    else:
+            bot.send_message(chat_id = update.message.chat_id, text="Goodbye!!")
 
 
+
+
+
+
+#
 #load the access token
 TOKEN = open('token.txt').read().strip()
 
