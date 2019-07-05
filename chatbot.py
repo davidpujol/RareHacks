3730ddf34f448030daafb9e7b737f5d96a4d401f
David Pujol <david.pujolperich@gmail.com>
2019-07-05 16:17:14 +0200

    Reads the token

--- chatbot.py
+++ chatbot.py
@@ -1,2 +1,19 @@
-#import the telegram api
 from telegram.ext import Updater
+from telegram.ext import MessageHandler, Filters
+
+#function to be called
+def responde(bot, update):
+    bot.send_message(chat_id = update.message.chat_id, text = "Hello World")
+
+
+#load the access token
+TOKEN = open('token.txt').read().strip()
+
+updater = Updater(token= TOKEN)
+dispatcher = updater.dispatcher
+
+#handling the call
+dispatcher.add_handler(MessageHandler(Filters.text, responde))
+
+#starting the bot
+updater.start_polling()
