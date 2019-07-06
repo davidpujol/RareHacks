deef73bf59062861c0a4ce22f60a2072fe870409
DelRiioo <sergidelriomartinez@gmail.com>
2019-07-06 13:44:41 +0200

    sergi

--- venv/chatbot.py
+++ venv/chatbot.py
@@ -212,6 +212,7 @@ SBCjXsQ99VWjbfwSFYh1UDv3QhzYfyGj
 
 
 
+
 #load the access token
 TOKEN = open('token.txt').read().strip()
 
@@ -226,5 +227,38 @@ updater.start_polling()
 
 
 
+
+
+
+
+# -------------------------------------
+"""
+import gensim
+import os
+import collections
+import smart_open
+import random
+
+test_data_dir = '{}'.format(os.sep).join([gensim.__path__[0], 'test', 'test_data'])
+lee_train_file = test_data_dir + os.sep + 'lee_background.cor'
+lee_test_file = test_data_dir + os.sep + 'lee.cor'
+
+def read_corpus(fname, tokens_only=False):
+    with smart_open.smart_open(fname, encoding="iso-8859-1") as f:
+        for i, line in enumerate(f):
+            if tokens_only:
+                yield gensim.utils.simple_preprocess(line)
+            else:
+                # For training data, add tags
+                yield gensim.models.doc2vec.TaggedDocument(gensim.utils.simple_preprocess(line), [i])
+
+train_corpus = list(read_corpus(lee_train_file))
+test_corpus = list(read_corpus(lee_test_file, tokens_only=True))
+
+model = gensim.models.doc2vec.Doc2Vec(vector_size=50, min_count=2, epochs=20)
+model.build_vocab(train_corpus)
+model.train(train_corpus, total_examples=model.corpus_count, epochs=model.epochs)
+
+"""
 
 
