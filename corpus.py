7a8a2543af3817d31a25e292ab077966fd58ef52
oriolaranda <oriolaranda@gmail.com>
2019-07-07 10:30:29 +0200

    oriol

--- corpus.py
+++ corpus.py
@@ -1,38 +1,132 @@
+#the drug used to treat intraocular melanoma is Aldesleukin
 
-import gensim
-import os
-import collections
-import smart_open
-import random
-import nltk
+import pandas as pd
+file = pd.read_csv('drugs_labs_biobanks_dataset.csv').values
 
-test_data_dir = '{}'.format(os.sep).join([gensim.__path__[0], 'test', 'test_data'])
-lee_train_file = test_data_dir + os.sep + 'lee_background.cor'
-lee_test_file = test_data_dir + os.sep + 'lee.cor'
-
-def read_corpus(fname, tokens_only=False):
-    with smart_open.open(fname, encoding="iso-8859-1") as f:
-        for i, line in enumerate(f):
-            if tokens_only:
-                yield gensim.utils.simple_preprocess(line)
-            else:
-                # For training data, add tags
-                yield gensim.models.doc2vec.TaggedDocument(gensim.utils.simple_preprocess(line), [i])
-
-train_corpus = list(read_corpus(lee_train_file))
-test_corpus = list(read_corpus(lee_test_file))
-
-print(len(train_corpus))
-
-model = gensim.models.doc2vec.Doc2Vec(vector_size=50, min_count=2, epochs=20)
-model.build_vocab(train_corpus)
-model.train(train_corpus, total_examples=model.corpus_count, epochs=model.epochs)
+names = file[:,0]
+dataset = []
+for i in range(23):
+    l = list(filter(lambda x: not pd.isnull(x), file[:,i]))
+    dataset.append(l)
 
 
-print(model.most_similar(['hello']))
 
-if not os.path.exists('models'):
-    os.makedirs('models')
-    model.save('models/doc2vec.model')
-else:
-    model.save('models/doc2vec.model')
+melanoma = "intraocular melanoma"
+
+def medical_name(list):
+    slist = []
+    for l in list:
+        s1 = "The drug used to treat "+melanoma+" is "+l+"."
+        slist.append(s1)
+        s2 = "Medicine prescribed for "+melanoma+" is "+l+"."
+        slist.append(s2)
+    return slist
+
+print(medical_name(dataset[0]))
+
+def brand_name(list):
+    slist = []
+    for l in dataset[0]:
+        s1= "The  brand name for " +l+" is "+ list[0]+"."
+        s2 = "The drug "+l+" is sold as "+list[0]+"."
+        slist.append(s1)
+        slist.append(s2)
+    return slist
+
+print(1,brand_name(dataset[1]))
+
+def drug_side_effects(list):
+    slist=[]
+    l = ", ".join(list)
+    s1 = "The side effects of the treatment are: "+l+"."
+    s2 = "This treatment can cause: "+ l +"."
+    slist.append(s1)
+    slist.append(s2)
+    return slist
+
+print(2,drug_side_effects(dataset[2]))
+def dangerous_side_effects(list):
+    slist=[]
+    l = ", ".join(list)
+    s1 = "If you have one of these side effects, you must go to your medical center: " + l + "."
+    s2 = "The following side effects are dangerous, please contact with your doctor: " + l + "."
+    slist.append(s1)
+    slist.append(s2)
+    return slist
+print(3,dangerous_side_effects(dataset[3]))
+def diagnostic_test(list):
+    slist=[]
+    l = ", ".join(list)
+    s1="The genetic tests for this illness are: "+l+"."
+    s2=l+" can be used for doing diagnose."
+    slist.append(s1)
+    slist.append(s2)
+    return slist
+
+print(4,diagnostic_test(dataset[14]))
+def patient_organisation(list):
+    slist=[]
+    l= ",".join(list)
+    s1= "You can put in contact with other patients in the following organisations: "+l+"."
+    s2 = "The following networks are people like you: "+l+"."
+    slist.append(s1)
+    slist.append(s2)
+    return slist
+
+print(5,patient_organisation(dataset[15]))
+
+def po_link(list):
+    slist=[]
+    l = zip(dataset[15],dataset[16])
+    for l1,l2 in l:
+        s = "This link "+l2+" corresponds to this organization: "+l1+"."
+        slist.append(s)
+    return slist
+
+print(6,po_link(dataset[16]))
+
+def age_onset(list):
+    slist=[]
+    if len(list) > 0:
+        l = ", ".join(list)
+        s1 ="The most common age of onset could be: "+l+"."
+        s2 = "The first signs appear in "+l+" age."
+        slist.append(s1)
+        slist.append(s2)
+    return slist
+
+print(7,age_onset(dataset[17]))
+
+def prevalence_eu(list):
+    slist=[]
+    if len(list)>0:
+        s="The prevalence of "+melanoma+" in Europe is "+list[0]+"."
+        slist.append(s)
+    return slist
+
+print(8,prevalence_eu(dataset[18]))
+
+def genes(list):
+    slist=[]
+    link="https://www.uniprot.org/"
+    for g,s,u in list:
+        s="The illness can be caused by a mutation in "+g+" ("+s+"), you find more information visiting "+link+" using this reference: "+u+"."
+        slist.append(s)
+    return slist
+
+ls = zip(dataset[19],dataset[20])
+
+ls = list(zip(ls,dataset[21]))
+
+print(ls)
+print(9,genes(ls))
+
+def symptoms(list):
+    #list = [veryfrequent,[l]]
+    slist = []
+    for sy,f in list:
+        s = f+"symptoms are: " +", ".join(sy)+"."
+        slist.append(s)
+    return slist
+
+print(10,symptoms(dataset[23:24]))
--- trainModel.py
+++ trainModel.py
@@ -1,11 +1,49 @@
 from gensim.test.utils import common_texts
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from gensim.test.utils import get_tmpfile
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy import spatial
+import nltk
 
-documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(common_texts)]
-model = Doc2Vec(documents, vector_size=5, window=2, min_count=1, workers=4)
 
 
-fname = get_tmpfile("my_doc2vec_model")
-model.save(fname)
-#model = Doc2Vec.load(fname)  # you can continue training with the loaded model!
+text = nltk.corpus.gutenberg.sents('melville-moby_dick.txt')
+
+
+#print(text)
+documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(text)]
+
+#print(common_texts)
+#documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(common_texts)]
+
+print(len(text))
+model = Doc2Vec(documents)
+#, vector_size=5, window=2, min_count=1, workers=4
+
+#model.save("models/doc2vec_model")
+#model = Doc2Vec.load("models/doc2vec_model")  # you can continue training with the loaded model!
+#print(model.most_similar(['Emma']))
+
+
+input = nltk.word_tokenize("whales are big".lower())
+input_v = model.infer_vector(input)
+
+#sentences = [ " ".join(w) for w in text]
+print(text[:5])
+llista = []
+for sen in text:
+    v = model.infer_vector(sen)
+    llista.append(v)
+
+print(len(llista))
+
+similituds = []
+i = 0
+for v in llista:
+    similituds.append([1-spatial.distance.cosine(input_v,v),i])
+    i+=1
+
+s = sorted(similituds, key=lambda x: x[0])
+
+
+print(text[s[-1][1]])
